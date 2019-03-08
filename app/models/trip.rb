50683fbea4f364e8ad21f8ee39f359cc667acdb5
gnik2036 <gnik203@gmail.com>
2019-03-08 05:42:08 +0200

    finish set constrains and finish task

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,12 +1,14 @@
-class Trip < ActiveRecord::Base
-  TRIP_STATUSES = %w(completed updated)
-  INVALID_STATUS_CHANGES = [['completed','ongoin']]
-  validates :status_validity, on: :update
+class Trip < ApplicationRecord
+  TRIP_STATUSES = %w[completed ongoin].freeze
+  INVALID_STATUS_CHANGES = [%w[completed ongoin]].freeze
+  validate :status_validity, on: :update
   validates :status, presence: true, inclusion: { in: TRIP_STATUSES }
 
-private
+  private
+
   def status_validity
     return unless status_changed?
+
     if INVALID_STATUS_CHANGES.include? status_change
       errors.add(:status, "Cannot be changed from #{status_was} to #{status}.")
     end
